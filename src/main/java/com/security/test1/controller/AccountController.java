f37d2832349cdb37ba31a7046aa56f508836a2c7
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-08-06 15:41:19 +0530

    Security test initiated

---
+++ src/main/java/com/security/test1/controller/AccountController.java
@@ -0,0 +1,18 @@
+package com.security.test1.controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/account")
+@CrossOrigin
+public class AccountController {
+    @GetMapping("/my-account")
+    public String getAccountDetails()
+    {
+        return "Here are the account details";
+    }
+
+
+}

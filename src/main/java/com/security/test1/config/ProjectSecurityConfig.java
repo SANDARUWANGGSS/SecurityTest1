631aafdca908bb6c53dd65ebe47c7c4957aa11a9
Sandaruwan Gamage <62364412+SANDARUWANGGSS@users.noreply.github.com>
2023-08-06 22:14:57 +0530

    Added In memory user creating

--- src/main/java/com/security/test1/config/ProjectSecurityConfig.java
+++ src/main/java/com/security/test1/config/ProjectSecurityConfig.java
@@ -3,7 +3,16 @@ package com.security.test1.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -15,4 +24,30 @@ public class ProjectSecurityConfig {
                 .and().formLogin().and().httpBasic();
         return http.build();
     }
+
+//    -----------------------In memory user created started--------------------
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager()
+//    {
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("12345")
+//                .authorities("admin")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin);
+//    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource)
+//    {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder()
+//    {
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+    //    -----------------------In memory user created ended--------------------
 }
